0eaa0aae7c28336e7dd7bd59d04d41089685c30c
Onah-NeLson <Onahnelson2@gmail.com>
2023-12-31 00:00:02 +0100

    integrating chatbot to site

--- client-side/scripts/chatbot.js
+++ client-side/scripts/chatbot.js
@@ -3,7 +3,13 @@ function chat(){
         "hi":"hello :)",
         "how are you":"Good :) You say?",
         "i'm also good":"How can i help you!",
-        "who are you":"I'm codewithMuazim",
+        "what location do you operate":"Currently operational in Nigeria",
+        "can i get produce by Way-bill":"Yes pls Way-Bill services available with charges",
+        "How much is a basket of potatoes":"1600 Naira per basket",
+        "who are you":"I'm 9ja-FarmYard Chatbot",
+        "who are you":"I'm 9ja-FarmYard Chatbot",
+        "who are you":"I'm 9ja-FarmYard Chatbot",
+        "who are you":"I'm 9ja-FarmYard Chatbot",
         "your favourite language":"Java, but most people hate it :(",
         "okay":"Thanku for being here!",
         "bye":"Okay! see you soon... :)",
@@ -14,6 +20,6 @@ function chat(){
         document.getElementById("answerBox").innerHTML=quetions[user] +"<br>";
     }
     else{
-        document.getElementById("answerBox").innerHTML="sorry, its not in my database";
+        document.getElementById("answerBox").innerHTML="sorry :( Not in my Database, check contact us page for more Enquiries..!!!";
     }
 }
